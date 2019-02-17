cc4c13ec5605e1f810db82ce991dc43e6545e745
princymishra <princymishra1997.5@gmail.com>
2019-02-17 15:13:50 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateField()
+    body  = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
